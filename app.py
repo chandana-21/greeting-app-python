05b862d988d8a25797a2de4f202853cc03744900
chandana-21 <chandanakuntala21@gmail.com>
2021-12-03 10:52:30 +0530

    edited

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
 app.secret_key = "chandana"
+
 @app.route("/hello", methods=['GET', 'POST'])
 def index():
 	flash("what's your name?")
